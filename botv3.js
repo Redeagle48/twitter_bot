9504f75452cdeb8d7decefcabcc6a43afaee7bbc
Redeagle48 <antoniopereira350@gmail.com>
2015-08-30 22:02:43 +0100

    Ready to test

--- botv3.js
+++ botv3.js
@@ -24,7 +24,7 @@ storage.initSync();
 //
 Bot.prototype.retweet = function (params, callback) {
     var self = this;
-    
+
     self.twit.get('search/tweets', params, function (err, reply) {
 
         // Get the hour when the operation is happening
@@ -37,67 +37,67 @@ Bot.prototype.retweet = function (params, callback) {
 
         // When there is an error with the request
         if (err) return callback(err);
-        
+
         // When there is no tweet returned in the search
         if (typeof reply === 'undefined' || reply.statuses.length === 0) {
             console.log("=> There **is no** tweet returned in the search.");
             return console.log("================= END =================\n");
         }
-        
+
         // Number of tweets in the buffer to be posted
         console.log("Before adding new tweets Buffer has length: " + tweets_buffer.length);
-        
+
         // Dump returned tweets
         var tweets = reply.statuses;
         console.log("=> Returned tweets from search: " + JSON.stringify(tweets.length));
         console.log("======================================");
-                
+
         // Add tweets fetched into the buffer to be posted
         tweets.forEach(function(element) {
             tweets_buffer.push(element);
         }, this);
-     
+
         // Number of tweets in the buffer to be posted
         console.log("After adding new tweets Buffer has length: " + tweets_buffer.length);
-   
-        // Execute 'processTweet' function after 'sched' time    
+
+        // Execute 'processTweet' function after 'sched' time
         if(tweets_buffer.length >= 1) {
-              
+
             processTweet();
 
-        } 
+        }
     });
 };
 
 function processTweet(){
-    
-    console.log("\n================= PROCESS TWEET =================");  
-    
+
+    console.log("\n================= PROCESS TWEET =================");
+
     // Extract the first tweet from the array
     var tweet = tweets_buffer.shift();
-    
+
     console.log("Original tweet -> " + tweet.text);
-    
+
     // Get the tweet text
     var tweet_text = tweet.text;
-    
-    // Generate md5 from the tweet text           
+
+    // Generate md5 from the tweet text
     var tweet_text_md5 = md5.md5(tweet_text);
-    
+
     //console.log("Processing tweet: " + tweet_text);
     console.log("Hash of the original Tweet -> " + tweet_text_md5);
-    
+
     if(storage.getItem(tweet_text_md5) === undefined) {
         // To post tweet
-        console.log("=> This tweet **was not** processed before");
-        
+        console.log("[ GOOD ] This tweet ** WAS NOT ** processed before");
+
         ////
         // Only accepts a tweet in english or portuguese
         if (tweet.metadata.iso_language_code === "en" ||
             tweet.metadata.iso_language_code === "pt" ||
             tweet.metadata.iso_language_code === "und") {
 
-            console.log("=> Tweet to be processed since language is \"en\" or \"pt\"");
+            console.log("[ GOOD ] Tweet to be processed since language is \"en\" or \"pt\"");
             console.log("======================================");
 
             //console.log("Tweet Text: " + tweet_text);
@@ -106,34 +106,35 @@ function processTweet(){
             var url = autolinker.link( tweet_text )
                                     .split("<a href=\"")[1]
                                     .split("\"")[0];
-            
+
             // Compose the Tweet and post it
             postTweet(tweet_text, url);
-        
+
         } else {
-            console.log("=> Rejected Tweet since language isn't \"en\" or \"pt\"");
+            console.log("[ BAD ] Rejected Tweet since language isn't \"en\" or \"pt\"");
         }
-        
+
         // Save tweet in the persistent storage in pair {tweet.text md5 -> tweet.text}
         storage.setItem(tweet_text_md5, tweet_text);
-       
+
     } else {
         // Tweet already posted, to ignore
-        console.log("=> This tweet **was** processed before");
+        console.log("[ BAD ] This tweet ** WAS ** processed before");
     }
-    
+
    console.log("================= END PROCESS TWEET =================\n");
-                
+
     // Until there are tweets to post
     if(tweets_buffer.length > 0){
-        later.setTimeout(processTweet, sched);
+        processTweet();
+        //later.setTimeout(processTweet, sched);
     }
 };
 
 function postTweet(tweet_text, url) {
-    
+
     var text_to_post = tweet_text;
-    
+
     if(url != ""){
         console.log("=> URL detected: " + url);
         console.log("======================================");
@@ -143,49 +144,49 @@ function postTweet(tweet_text, url) {
                 console.log("Couldn’ t get page because of error: " + error);
                 return;
             }
-            
+
             var $ = cheerio.load(body);
-            
+
             // to be used when processing tweets to compose the tweet to post
             var company, job, location = "";
-            
+
             // Is ITJobs
             if($('meta[name=application-name]').attr('content') === "ITJobs") {
                 console.log("Tweet from IT Jobs");
-                
+
                 company = $('div[class="company"] a').attr('title');
                 job = $('div[class="section"] h1').text();
                 job = job.split("(")[0].split(")")[0];
-                
+
                 //console.log("Job: " + job + "; Company: " + company);
                 text_to_post = "Job: " + job + "; Company: " + company + " " + url;
-               
+
                 // THIS FOLLOWING CODE WAS NOT BEING PROCESSED RIGHT WHEN IN THE END OF THE IF
-                
+
                 // Add hashtag to the tweet to post
                 text_to_post += " #outsystems";
                 // Post tweet
                 //console.log("Tweet to post: " + text_to_post);
                 TweetPost(text_to_post);
-               
+
             }
             // Is Expresso Emprego?
             else if($('meta[name=Author]').attr('content') === "Expresso Emprego") {
                 console.log("Tweet from Expresso Jobs");
-                
+
                 company = $('h2[class="xslTitulo"]').text().split("Empresa:")[1].trim();
-                
+
                 job = $('h1[class="back h xslTitulo"]');
                 job = job.text().split("\t")[0].split("(m")[0];
 
                 location = $('h1[class="back h xslTitulo"]').children().text();
-                
+
                 //console.log("Job: " + job + "; Company: " + company + "; Location: " + location);
-                text_to_post = "Job: " + job + "; Company: " + company + "; Location: " + location 
+                text_to_post = "Job: " + job + "; Company: " + company + "; Location: " + location
                     + " " + url;
-                    
+
                // THIS FOLLOWING CODE WAS NOT BEING PROCESSED RIGHT WHEN IN THE END OF THE IF
-                
+
                 // Add hashtag to the tweet to post
                 text_to_post += " #outsystems";
                 // Post tweet
@@ -195,41 +196,44 @@ function postTweet(tweet_text, url) {
             // Is Sapo Emprego?
             else if($('meta[name=author]').attr('content') === "Sapoemprego") {
                 console.log("Tweet from Sapo Emprego");
-                
+
                 company = $('div[class=resumo] div[class=offerbox] dl dd a').closest().text();
-                
+
                 job = $('div[id=boxAnuncio] h2[style="float: left;"]').text();
-                
+
                 //location = $('div[class=resumo] div[class=offerbox] dl dd a')[2].text() + " - "
                 //    + $('div[class=resumo] div[class=offerbox] dl dd a')[1].text();
-                
-                
+
+
             }
             // Any other source
             else {
                 // no additional process happening
             }
-            
+
             // When the tweet was processed, compose the tweet to post
             if(company != "") {
                 // TODO:
                 // 1) What to do when bigger than 140 charecters
-                text_to_post = job.trim() + 
+                text_to_post = tweet_text;
+                                        /*
+                                        "DUMMY"+//job.trim() +
                                         ((company != "") ? " (" + company + ")" : "") +
                                         " -" +
                                         ((location.trim() != "") ? location.trim() : "") +
                                         " " + url;
-                
+                                        */
+
                 // THIS FOLLOWING CODE WAS NOT BEING PROCESSED RIGHT WHEN IN THE END OF THE IF
-                
+
                 // Add hashtag to the tweet to post
-                text_to_post += " #outsystems";
+                text_to_post += " @OutSystems";
                 // Post tweet
                 //console.log("Tweet to post: " + text_to_post);
                 TweetPost(text_to_post);
             }
         });
-    } 
+    }
     // No url in the tweet text
     else {
         console.log("URL not detected");
@@ -249,9 +253,9 @@ function postTweet(tweet_text, url) {
 function TweetPost(text_to_post){
     console.log("Tweet to Post: " + text_to_post);
     // Post
-    //self.twit.post('statuses/update', {
-    //    status: text_to_post
-    //}, tweetcallback);
+    self.twit.post('statuses/update', {
+        status: text_to_post
+    }, tweetcallback);
 }
 
 //
--- outsystemsbotv3.js
+++ outsystemsbotv3.js
@@ -1,72 +1,77 @@
 var Bot = require("./botv3"),
-    config1 = require("./config1");
+  config1 = require("./config1");
 
 var bot = new Bot(config1);
 
 //get date string for today's date (e.g. 2011-01-01)
 function datestring() {
-    var d = new Date(Date.now() - 5 * 60 * 60 * 1000); //est timezone
-    return d.getUTCFullYear() + "-" +
-        (d.getUTCMonth() + 1) + "-" +
-        (d.getDate() - 2); // Looking for tweets in the last 2 days
+  var d = new Date(Date.now() - 5 * 60 * 60 * 1000); //est timezone
+  return d.getUTCFullYear() + "-" +
+    (d.getUTCMonth() + 1) + "-" +
+    (d.getDate() - 2); // Looking for tweets in the last 2 days
 };
 
 // queries
-    var query_developer = "outsystems developer",
-        query_programmer = "outsystems programmer",
-        query_consultant = "outsystems consultant",
-        query_consultor = "outsystems consultor";
-        
-        
+var query_developer = "outsystems developer",
+  query_programmer = "outsystems programmer",
+  query_consultant = "outsystems consultant",
+  query_consultor = "outsystems consultor";
+
+
 console.log("Twitter bot running.....");
 
-setInterval(function () {
-    	
-    console.log("\n================= START =================");    
-        
-    // Generate random number
-    var rand = Math.random();    
-    
-    //executeTweet(query_consultor);
-    
-    
-    switch(true) {
-        case rand <= .25:
-            //console.log(".25"); break;
-            executeTweet(query_developer); break;
-        case rand <= .50:
-            //console.log(".5"); break;
-            executeTweet(query_programmer); break;
-        case rand <= .75:
-            //console.log(".75"); break;
-            executeTweet(query_consultant); break;
-        case rand <= 1:
-            //console.log("1"); break;
-            executeTweet(query_consultor); break;
-        default:
-            console.log("Random value outside boundaries (rand = " + rand + ")."); break;
-    }
-    
+setInterval(function() {
 
-}, 120000/*miliseconds*/);
+  console.log("\n================= START =================");
+
+  // Generate random number
+  var rand = Math.random();
+
+  //executeTweet(query_consultor);
+
+
+  switch (true) {
+    case rand <= .25:
+      //console.log(".25"); break;
+      executeTweet(query_developer);
+      break;
+    case rand <= .50:
+      //console.log(".5"); break;
+      executeTweet(query_programmer);
+      break;
+    case rand <= .75:
+      //console.log(".75"); break;
+      executeTweet(query_consultant);
+      break;
+    case rand <= 1:
+      //console.log("1"); break;
+      executeTweet(query_consultor);
+      break;
+    default:
+      console.log("Random value outside boundaries (rand = " + rand + ").");
+      break;
+  }
+
+
+}, 300000/*120000 /*miliseconds*/ );
 
 function executeTweet(query) {
-    var params = {
-            q: query,
-            since: datestring(),
-            result_type: "mixed"
-        };
+  var params = {
+    q: query,
+    since: datestring(),
+    result_type: "mixed"
+  };
 
-        bot.retweet(params, function(err, reply) {
-            if(err) return handleError(err);
+  bot.retweet(params, function(err, reply) {
+    if (err) return handleError(err);
 
-        });
+  });
 }
 
 //
 // When there is an error with the request
 //
 function handleError(err) {
-    console.error("response status:", err.statusCode);
-    console.error("data:", err.data);
+  console.error("response status:", err.statusCode);
+  console.error("data:", err.data);
 }
